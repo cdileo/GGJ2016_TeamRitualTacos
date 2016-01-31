e1d441025c93b0daa4cf672416f93bacd2cd9bd3
M Findlay-Olynyk <M Findlay-Olynyk>
2016-01-31 14:56:20 -0800

    added credits

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour
+{
+    GameObject levelMan;
+
+    public Text textBox;
+    private string[] story = { "Credits:\n   Art: James Choi\n   Programming: Chris Dileo, Roxanne Taylor, Michelle Findlay-Olynyk\n   Narrative & Mechanics Design: Michelle Findlay-Olynyk\n   Sound: Evan Witt"};
+    private int position;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = 0;
+        textBox.text = story[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NewMessage();
+        }
+    }
+
+    public void NewMessage()
+    {
+        if (position < story.Length - 1)
+        {
+            textBox.text = story[++position];
+        }
+        else {
+            levelMan = GameObject.Find("LevelManager");
+            (levelMan.GetComponent<LevelManagerScript>()).loadSplashScreen();
+        }
+    }
+}
