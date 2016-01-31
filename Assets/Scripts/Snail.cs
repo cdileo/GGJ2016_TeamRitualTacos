a03a01c4978adff9bc6bb13ed665634ec53827a8
M Findlay-Olynyk <M Findlay-Olynyk>
2016-01-31 10:32:50 -0800

    Adding snail mechanics

---
+++ Assets/Scripts/Snail.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snail : MonoBehaviour {
+
+    public int health;
+    public int defense = 0;
+
+    private int maxHealth = 10;
+    private float nearThreshold = 3f;
+
+    
+	public void NearMechanics(int stage, Vector3 tPos, Vector3 mPos)
+	{
+        switch (stage)
+        {
+            case 0:
+                EyeButt(tPos, mPos);
+                break;
+
+            case 1:
+                Hide();
+                break;
+
+            case 2:
+                EyeButt(tPos, mPos);
+                break;
+
+            default:
+                EyeButt(tPos, mPos);
+                break;
+        }
+
+
+	}
+
+
+    public void FarMechanics(int stage, Vector3 tPos, Vector3 mPos)
+    {
+        switch (stage)
+        {
+            case 0:
+                //do nothing for now
+                //ranged if time
+
+                break;
+
+            case 1:
+                Hide();
+
+                break;
+
+            default:
+                //do nothing for now
+                break;
+        }
+
+
+    }
+
+
+    public void EyeButt(Vector3 tPos, Vector3 mPos)
+    {
+        SendMessageUpwards("SnailDefense", 0);
+        //query characters to see if either get hit
+        //if so, get 1 damage
+        if (Vector2.Distance(tPos, transform.position) < nearThreshold)
+            SendMessageUpwards("DamageToTurtle", 1);
+        if (Vector2.Distance(mPos, transform.position) < nearThreshold)
+            SendMessageUpwards("DamageToMouse", 1);
+        
+    }
+
+
+    public void Hide()
+    {
+        SendMessageUpwards("SnailDefense", 2);
+        //animate?
+        //sound effect?
+
+
+    }
+
+
+}
--- Assets/Scripts/bossScript.cs
+++ Assets/Scripts/bossScript.cs
@@ -5,20 +5,42 @@ public class bossScript : MonoBehaviour {
 
     public GameObject turtle;
     public GameObject rabbit;
+    public Snail snail1;
     public float attackInterval;
     public float nearThreshold = 3f;
+    public int level;
+    
 
     private float lastAttackTime;
     private bool playerNear;
+    private int stage;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         playerNear = checkNear();
+
+        switch (level)
+        {
+            case 1:
+                if (playerNear)
+                    snail1.NearMechanics(stage, turtle.transform.position, rabbit.transform.position);
+                else
+                    snail1.FarMechanics(stage, turtle.transform.position, rabbit.transform.position);
+                break;
+
+
+
+            default:
+                break;
+        }
+
+       
+        stage = (stage + 1) % 6;
 	}
 
     private bool checkNear()
