a7fe610561d3d43de88080f06da4dcd01f00c09a
M Findlay-Olynyk <M Findlay-Olynyk>
2016-01-31 13:21:10 -0800

    added story controllers for all text cut scenes

---
+++ Assets/Scripts/StoryController1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoryController1 : MonoBehaviour
+{
+
+
+    public Text textBox;
+    private string[] story = { "Turtle: \"Mouse! Mouse! Star Platypus has been kidnapped by the Octpus King! We have to rescue them!\"", 
+            "Mouse: \"I know you're hard to hurt Turtle, but the tunnel to the Octopus King's lair will be well defended. I can throw a knife, but if I get hit I'm done for.\"",
+            "Turtle: \"C'mon, just stay behind me and I'll protect you. Just jump out to attack! Together we can defeat them and bring back the amazing Star Platypus!\"",
+            "Mouse: \"... Right, amazing.... Well, together sounds good at least.\""};
+    private int position;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = 0;
+        textBox.text = story[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewMessage()
+    {
+        if (position < story.Length - 1)
+        {
+            textBox.text = story[++position];
+        }
+        else {
+            // TODO: move to next scene
+        }
+    }
+}
---
+++ Assets/Scripts/StoryController3.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoryController3 : MonoBehaviour
+{
+
+
+    public Text textBox;
+    private string[] story = { "Octopus: \"So, you scrawny miscreants made it all the way here.\"",
+            "Turtle: \"Let Star Platypus go you monster! We aren't responsible for the plague, and Star Platypus was just trying to help your kingdom!\"",
+            "Octopus: \"Oh, I know that! Who cares if a few die? I've found something far better: a ritual to steal Star Playpus's power so all my creatures can survive in the air. We will take over your precious land! Ha ha ha ha ha!\"",
+            "Mouse: \"Oh no! We have to stop them!\""};
+    private int position;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = 0;
+        textBox.text = story[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewMessage()
+    {
+        if (position < story.Length - 1)
+        {
+            textBox.text = story[++position];
+        }
+        else {
+            // TODO: move to next scene
+        }
+    }
+}
